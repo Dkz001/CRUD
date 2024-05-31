f27f4aa9fa15e32dda1960493915fc9b05ab7ec5
Jonathan DIki <jonathandiki@outlook.com>
2024-05-31 03:57:13 +0200

    new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 //  import React from 'react'
 import { useState, useRef, KeyboardEvent } from "react";
+import Modal from "./components/Modal"
+import Trash from "./icons/Trash"
 
 function App() {
+    const [open, setOpen] = useState(false)
   const inputRef = useRef();
   const [compteur, setCompteur] = useState(1);
   const [fruits, setFruits] = useState([
@@ -79,6 +82,32 @@ function App() {
       </form>
       {/* <button onClick={handleClick}>Incrémente</button> */}
     </div>
+
+
+       <button className="btn btn-danger" onClick={() => setOpen(true)}>
+        <Trash /> Delete
+      </button>
+
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div className="text-center w-56">
+          <Trash size={56} className="mx-auto text-red-500" />
+          <div className="mx-auto my-4 w-48">
+            <h3 className="text-lg font-black text-gray-800">Confirm Delete</h3>
+            <p className="text-sm text-gray-500">
+              Are you sure you want to delete this item?
+            </p>
+          </div>
+          <div className="flex gap-4">
+            <button className="btn btn-danger w-full">Delete</button>
+            <button
+              className="btn btn-light w-full"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Modal>
   );
 }
 export default App;
